53ef58170ef7240a10496fc49505611f04dfb39c
qamarfj <qamarfj@hotmail.com>
2020-11-28 17:29:23 +0100

    implemented feedback

--- javascript/javascript2/week1/shortestWord/shortest-word.js
+++ javascript/javascript2/week1/shortestWord/shortest-word.js
@@ -1,15 +1,15 @@
-"use strict"
+"use strict";
 /**Write a function that finds the shortest word of an array of words */
 const danishWords = ["bil", "plante", "kaffe", "bog", "ø", "planetarium"];
 
 function getShortestWord(danishWords) {
-    let shortestWord = danishWords[0];
-    for (const currentWord of danishWords) {
-        if (shortestWord.length > currentWord.length) {
-            shortestWord = currentWord;
-        }
+  let shortestWord = danishWords[0];
+  for (let i = 1; i < danishWords.length; i++) {
+    if (shortestWord.length > danishWords[i].length) {
+      shortestWord = danishWords[i];
     }
-    return shortestWord;
+  }
+  return shortestWord;
 }
 const shortestWord = getShortestWord(danishWords); // returns 'ø'
 console.log(shortestWord);
