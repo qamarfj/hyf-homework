053e01ab923ec373183e77ed16d50851c3a97029
qamarfj <qamarfj@hotmail.com>
2021-03-08 10:40:07 +0100

    added fetching fom API,  description , deadline and update

--- react/week3/todos/src/TodosList.js
+++ react/week3/todos/src/TodosList.js
@@ -1,30 +1,70 @@
-function TodoItem({ todo, DeleteTodo, toggleDone }) {
+import { useState } from "react";
+
+function TodoItem({ todo, DeleteTodo, UpdateTodo }) {
+  const [isCheked, setIsCheked] = useState(false);
+  const [isUpdated, setIsUpdated] = useState(true);
+  const [todoDescription, settodoDescription] = useState(todo.description);
+  const updateDescription = (e) => {
+    settodoDescription(e.target.value);
+  };
   return (
-    <li className={todo.isDone ? "Done" : "notDone"}>
-      {todo.description}
+    <li className={isCheked ? "Done" : "notDone"}>
+      {isUpdated && (
+        <>
+          {todo.description} | {todo.deadline}
+        </>
+      )}
+
+      <>
+        {!isUpdated && (
+          <input
+            type="text"
+            value={todoDescription}
+            onChange={updateDescription}
+            required
+          />
+        )}
+      </>
       <input
         type="checkbox"
-        checked={todo.isDone}
-        onChange={() => toggleDone(todo.id)}
+        checked={isCheked}
+        onChange={() => setIsCheked(!isCheked)}
       />
       <button onClick={() => DeleteTodo(todo.id)}>Delete</button>
+      {isUpdated && (
+        <button onClick={() => setIsUpdated(!isUpdated)}>Edit</button>
+      )}
+      {!isUpdated && (
+        <button
+          disabled={!todoDescription}
+          onClick={() => {
+            setIsUpdated(!isUpdated);
+            UpdateTodo(todo.id, todoDescription);
+          }}
+        >
+          Update
+        </button>
+      )}
     </li>
   );
 }
 
-export default function TodosList({ todos, DeleteTodo, toggleDone }) {
-  if (todos.length > 0)
-    return (
-      <ul>
-        {todos.map((todo) => (
-          <TodoItem
-            key={todo.id}
-            todo={todo}
-            DeleteTodo={DeleteTodo}
-            toggleDone={toggleDone}
-          />
-        ))}
-      </ul>
-    );
-  else return <div> No Items</div>;
+export default function TodosList({ todos, DeleteTodo, UpdateTodo }) {
+  return (
+    <>
+      {todos.length > 0 && (
+        <ul>
+          {todos.map((todo) => (
+            <TodoItem
+              key={todo.id}
+              todo={todo}
+              DeleteTodo={DeleteTodo}
+              UpdateTodo={UpdateTodo}
+            />
+          ))}
+        </ul>
+      )}
+      {todos.length < 1 && <div> No Items</div>}
+    </>
+  );
 }
--- react/week3/todos/src/todos.js
+++ react/week3/todos/src/todos.js
@@ -1,17 +1,115 @@
-export const todos = [
-  {
-    id: 1,
-    description: "Get out of bed",
-    isDone: false,
-  },
-  {
-    id: 2,
-    description: "Brush teeth",
-    isDone: false,
-  },
-  {
-    id: 3,
-    description: "Eat breakfast",
-    isDone: false,
-  },
-];
+import TodosList from "./TodosList";
+import { useEffect, useState } from "react";
+const URL =
+  "https://gist.githubusercontent.com/benna100/391eee7a119b50bd2c5960ab51622532/raw";
+
+export default function Todos() {
+  const [currentTodos, setCurrentTodos] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [description, setDescription] = useState("");
+  const [deadline, setDeadline] = useState("");
+
+  useEffect(() => {
+    const fetchAPI = () => {
+      fetch(URL)
+        .then((response) => response.json())
+        .then((todos) => {
+          setIsLoaded((prev) => !prev);
+          setCurrentTodos((prev) => prev.concat(todos));
+        });
+    };
+    fetchAPI();
+    return () => {
+      setIsLoaded(() => {
+        return false;
+      });
+    };
+  }, []);
+
+  function AddTodo(e) {
+    const isValidDate = Date.parse(deadline) >= Date.now();
+    if (!isValidDate) {
+      console.log("not valid date");
+    }
+    if (description !== "" && isValidDate) {
+      setCurrentTodos((prev) => [
+        ...prev,
+        {
+          id: currentTodos.length + 1,
+          description: description,
+          deadline: deadline,
+        },
+      ]);
+      setDescription("");
+      setDeadline("");
+    }
+    e.preventDefault();
+  }
+  const DeleteTodo = (id) => {
+    const todos = [...currentTodos];
+    setCurrentTodos(todos.filter((todo) => todo.id !== id));
+  };
+  const UpdateTodo = (id, description) => {
+    const todos = [...currentTodos];
+    setCurrentTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          todo.description = description;
+        }
+        return todo;
+      })
+    );
+  };
+  const updateDescription = (e) => {
+    setDescription(e.target.value);
+  };
+
+  const updateDeadline = (e) => {
+    setDeadline(e.target.value);
+  };
+
+  function minDate() {
+    const now = new Date();
+    const month =
+      now.getMonth() > 9 ? now.getMonth() + 1 : "0" + (now.getMonth() + 1);
+    const day = now.getDate() > 9 ? now.getDate() : "0" + now.getDate();
+    const currentDate = "" + now.getFullYear() + "-" + month + "-" + day;
+    return currentDate;
+  }
+
+  return (
+    <div>
+      <form onSubmit={AddTodo}>
+        <label htmlFor="description">Todo Description : </label>
+        <input
+          type="text"
+          name="description"
+          id="description"
+          value={description}
+          onChange={updateDescription}
+          required
+        />
+        <label htmlFor="deadline">Deadline : </label>
+        <input
+          type="date"
+          name="deadline"
+          id="deadline"
+          value={deadline}
+          onChange={updateDeadline}
+          min={minDate()}
+          required
+        />
+        <button type="submit">Add Todo</button>
+      </form>
+      {isLoaded ? (
+        <TodosList
+          todos={currentTodos}
+          DeleteTodo={DeleteTodo}
+          UpdateTodo={UpdateTodo}
+        />
+      ) : (
+        <h2>Loading.....</h2>
+      )}
+    </div>
+  );
+}
